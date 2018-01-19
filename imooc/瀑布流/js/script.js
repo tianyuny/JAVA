085f6fc4907ac35444b3fe81d1174f8a95c908de
yijingjun <1698233847@qq.com>
2018-01-19 15:01:42 +0800

    2018-1-19 15:01:36

--- imooc/瀑布流/js/script.js
+++ imooc/瀑布流/js/script.js
@@ -2,71 +2,28 @@ window.onload=function(){
     var warp=$('#container'),
         box=warp.children('div');
     waterfall(warp,box);
-    var data = [{
-        "src": "1.png",
-        "title": "第一怪 竹筒当烟袋"
-    }, {
-        "src": "2.png",
-        "title": "第二怪 草帽当锅盖"
-    }, {
-        "src": "3.png",
-        "title": "第三怪 这边下雨那边晒"
-    }, {
-        "src": "4.png",
-        "title": "第四怪 四季服装同穿戴"
-    }, {
-        "src": "5.png",
-        "title": "第五怪 火车没有汽车快"
-    }, {
-        "src": "6.png",
-        "title": "第六怪 火车不通国内通国外"
-    }, {
-        "src": "7.png",
-        "title": "第七怪 老奶爬山比猴快"
-    }, {
-        "src": "8.png",
-        "title": "第八怪 鞋子后面多一块"
-    }, {
-        "src": "9.png",
-        "title": "第九怪 脚趾四季露在外"
-    }, {
-        "src": "10.png",
-        "title": "第十怪 鸡蛋拴着卖"
-    }, {
-        "src": "11.png",
-        "title": "第十一怪 粑粑叫饵块"
-    }, {
-        "src": "12.png",
-        "title": "第十二怪 花生蚕豆数着卖"
-    }, {
-        "src": "13.png",
-        "title": "第十三怪 三个蚊子一盘菜"
-    }, {
-        "src": "14.png",
-        "title": "第十四怪 四个老鼠一麻袋"
-    }, {
-        "src": "15.png",
-        "title": "第十五怪 树上松毛扭着卖"
-    }, {
-        "src": "16.png",
-        "title": "第十六怪 姑娘叫老太"
-    }, {
-        "src": "17.png",
-        "title": "第十七怪 小和尚可以谈恋爱"
-    }, {
-        "src": "18.png",
-        "title": "第十八怪 背着娃娃谈恋爱"
-    }];
     //追加图片
     $(document).scroll(function(){
        if($(this).scrollTop()+$(window).height()>=$(this).height()){
            for(var i=0;i<data.length;i++){
-               warp.append('<div><a href="#"><img src="img/'+data[i].src+'" alt=""><span>'+data[i].title+'</span></a></div>');
+               box.push($('<div><a href="#"><img src="img/'+data[i].src+'" alt=""><span>'+data[i].title+'</span></a></div>').get(0));
+               warp.append(box.eq(box.length-1));
            }
-           // console.log(box.length);
-           box=warp.children('div');
            waterfall(warp,box);
        }
+    });
+    //浏览器窗口大小改变
+    $(window).resize(function(){
+        boxI=0;
+        num=0;
+        var boxW = (box.outerWidth(true)),
+            colls = Math.floor($(window).width()/boxW);
+        for(var i=0;i<colls;i++){
+            box.eq(i).css("position", "static");
+        }
+        boxH=[];
+        waterfall(warp,box);
+        box.stop(false,true);
     });
 };
 //瀑布流主函数
@@ -108,6 +65,61 @@ function boxStyle(box,top,left,index){
         "opacity": 0
     }).stop().animate({
         "opacity": 1
-    },1000);
+    },500);
     num=index;
 }
+var data = [{
+    "src": "1.png",
+    "title": "第一怪 竹筒当烟袋"
+}, {
+    "src": "2.png",
+    "title": "第二怪 草帽当锅盖"
+}, {
+    "src": "3.png",
+    "title": "第三怪 这边下雨那边晒"
+}, {
+    "src": "4.png",
+    "title": "第四怪 四季服装同穿戴"
+}, {
+    "src": "5.png",
+    "title": "第五怪 火车没有汽车快"
+}, {
+    "src": "6.png",
+    "title": "第六怪 火车不通国内通国外"
+}, {
+    "src": "7.png",
+    "title": "第七怪 老奶爬山比猴快"
+}, {
+    "src": "8.png",
+    "title": "第八怪 鞋子后面多一块"
+}, {
+    "src": "9.png",
+    "title": "第九怪 脚趾四季露在外"
+}, {
+    "src": "10.png",
+    "title": "第十怪 鸡蛋拴着卖"
+}, {
+    "src": "11.png",
+    "title": "第十一怪 粑粑叫饵块"
+}, {
+    "src": "12.png",
+    "title": "第十二怪 花生蚕豆数着卖"
+}, {
+    "src": "13.png",
+    "title": "第十三怪 三个蚊子一盘菜"
+}, {
+    "src": "14.png",
+    "title": "第十四怪 四个老鼠一麻袋"
+}, {
+    "src": "15.png",
+    "title": "第十五怪 树上松毛扭着卖"
+}, {
+    "src": "16.png",
+    "title": "第十六怪 姑娘叫老太"
+}, {
+    "src": "17.png",
+    "title": "第十七怪 小和尚可以谈恋爱"
+}, {
+    "src": "18.png",
+    "title": "第十八怪 背着娃娃谈恋爱"
+}];
